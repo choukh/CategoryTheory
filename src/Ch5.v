417af4d5d00e3ba6b2121fc04573a77f7ee44b05
choukh <choukyuhei@gmail.com>
2022-08-31 02:07:50 +0900

    coq_well_pointed

--- src/Ch5.v
+++ src/Ch5.v
@@ -1,8 +1,9 @@
 (*** Coq coding by choukh, Aug 2022 ***)
 
 Require Import Lib.
-From Category.Theory Require Import Category.
+From Category.Theory Require Morphisms.
 From Category.Construction Require Isomorphism Slice.
+From Category.Instance Require Coq.
 From Category.Structure Require Terminal.
 
 (** 5.1 **)
@@ -60,6 +61,15 @@ Next Obligation. rewrite id_left in X0, X. now rewrites. Defined.
 End Ch5_2.
 
 (** 5.3 **)
+Section Ch5_3.
+Import Coq.
+
+Fact coq_well_pointed (B C : Coq) (f g : B ~> C) :
+  f ≈ g ↔ ∀ b : 1 ~> B, f ∘ b ≈ g ∘ b.
+Proof. split.
+  - intros eq A. now rewrites.
+  - intros H x. specialize H with (λ _, x). simpl in H. tauto.
+Qed.
 
 Fact arrow_extensionality {ℂ : Category} (B C : ℂ) (f g : B ~> C) :
   f ≈ g ↔ ∀ A (x: A ~> B), f ∘ x ≈ g ∘ x.
@@ -67,3 +77,22 @@ Proof. split.
   - intros eq A x. now rewrites.
   - intros H. specialize H with B id. now rewrite !id_right in H.
 Qed.
+
+End Ch5_3.
+
+(** 5.4 **)
+Section Ch5_4.
+Import Morphisms Coq.
+
+Variable B C : Coq.
+Variable f : B ~> C.
+
+Fact coq_monic : Monic f ↔ ∀ x y, f x = f y → x = y.
+Proof. split.
+  - intros [monic] x y eq.
+    specialize monic with 1 (λ _, x) (λ _, y).
+    simpl in monic. tauto.
+  - intros inj. construct. apply inj, H.
+Qed.
+
+End Ch5_4.
